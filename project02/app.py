af4690ea6f188612e7a94393411e9fba3832ac58
Yoon321 <tkdwls734@gmail.com>
2022-05-02 23:04:59 +0900

    초기 API 설계

--- project02/app.py
+++ project02/app.py
@@ -1,14 +1,60 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from pymongo import MongoClient
+import requests
 
 app = Flask(__name__)
 
+client = MongoClient('52.79.226.1', 27017, username="test", password="test")
+db = client.dbsparta_plus_week2
+
 @app.route('/')
 def main():
+    # DB에서 저장된 단어 찾아서 HTML에 나타내기
     return render_template("index.html")
 
-@app.route('/detail')
-def detail():
-    return render_template("detail.html")
+
+@app.route('/detail/<keyword>')
+def detail(keyword):
+    # API에서 단어 뜻 찾아서 결과 보내기
+    return render_template("detail.html", word=keyword)
+
+
+@app.route('/api/save_word', methods=['POST'])
+def save_word():
+    # 단어 저장하기
+    return jsonify({'result': 'success', 'msg': '단어 저장'})
+
+
+@app.route('/api/delete_word', methods=['POST'])
+def delete_word():
+    # 단어 삭제하기
+    return jsonify({'result': 'success', 'msg': '단어 삭제'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)@app.route('/')
+def main():
+    # DB에서 저장된 단어 찾아서 HTML에 나타내기
+    return render_template("index.html")
+
+
+@app.route('/detail/<keyword>')
+def detail(keyword):
+    # API에서 단어 뜻 찾아서 결과 보내기
+    return render_template("detail.html", word=keyword)
+
+
+@app.route('/api/save_word', methods=['POST'])
+def save_word():
+    # 단어 저장하기
+    return jsonify({'result': 'success', 'msg': '단어 저장'})
+
+
+@app.route('/api/delete_word', methods=['POST'])
+def delete_word():
+    # 단어 삭제하기
+    return jsonify({'result': 'success', 'msg': '단어 삭제'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
