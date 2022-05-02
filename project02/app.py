7ce4c51fd5ca90037658f407f7ede91c6c79bfd5
Yoon321 <tkdwls734@gmail.com>
2022-05-03 00:33:15 +0900

    단어 저장, 삭제 기능 완성

--- project02/app.py
+++ project02/app.py
@@ -23,41 +23,28 @@ def detail(keyword):
     return render_template("detail.html", word=keyword, result=result, status = status_receive)
 
 
+
 @app.route('/api/save_word', methods=['POST'])
 def save_word():
     # 단어 저장하기
-    return jsonify({'result': 'success', 'msg': '단어 저장'})
+    word_receive = request.form['word_give']
+    definition_receive = request.form['definition_give']
+
+    doc = {
+        'word': word_receive,
+        'definition': definition_receive
+    }
+    print(doc)
+    db.words.insert_one(doc)
+    return jsonify({'result': 'success', 'msg': f'{word_receive} 저장'})
 
 
 @app.route('/api/delete_word', methods=['POST'])
 def delete_word():
     # 단어 삭제하기
-    return jsonify({'result': 'success', 'msg': '단어 삭제'})
-
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)@app.route('/')
-def main():
-    # DB에서 저장된 단어 찾아서 HTML에 나타내기
-    return render_template("index.html")
-
-
-@app.route('/detail/<keyword>')
-def detail(keyword):
-    # API에서 단어 뜻 찾아서 결과 보내기
-    return render_template("detail.html", word=keyword)
-
-
-@app.route('/api/save_word', methods=['POST'])
-def save_word():
-    # 단어 저장하기
-    return jsonify({'result': 'success', 'msg': '단어 저장'})
-
-
-@app.route('/api/delete_word', methods=['POST'])
-def delete_word():
-    # 단어 삭제하기
-    return jsonify({'result': 'success', 'msg': '단어 삭제'})
+    word_receive = request.form['word_give']
+    db.words.delete_one({"word":word_receive})
+    return jsonify({'result': 'success', 'msg': f'{word_receive} 삭제'})
 
 
 if __name__ == '__main__':
