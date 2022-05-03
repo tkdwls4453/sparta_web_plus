5f5629ba95320185c3f8ef792ca58821b9661adf
Yoon321 <tkdwls734@gmail.com>
2022-05-03 10:29:45 +0900

    최종 완성
    검색 기능 추가, 사전에 없는 단어일 경우 알림 추가, og태그 및 favicon 추가

--- project02/app.py
+++ project02/app.py
@@ -10,8 +10,9 @@ db = client.dbsparta_plus_week2
 @app.route('/')
 def main():
     # DB에서 저장된 단어 찾아서 HTML에 나타내기
-    words = db.words.find({},{'_id': False})
-    return render_template("index.html", words = words)
+    words = list(db.words.find({},{'_id': False}))
+    msg = request.args.get('msg')
+    return render_template("index.html", words = words, msg=msg)
 
 
 @app.route('/detail/<keyword>')
@@ -19,8 +20,11 @@ def detail(keyword):
     # API에서 단어 뜻 찾아서 결과 보내기
     r = requests.get(f"https://owlbot.info/api/v4/dictionary/{keyword}",headers={"Authorization": "Token 868b9b2ac3e666089546c855ae5672b5b8205645"})
     result = r.json()
-    status_receive = request.args.get('status_give', 'new')
 
+    if r.status_code != 200:
+        return redirect(url_for('main', msg='Word not found in dictionary; Try another word'))
+
+    status_receive = request.args.get('status_give', 'new')
     return render_template("detail.html", word=keyword, result=result, status = status_receive)
 
 
