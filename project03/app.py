a7c42f58706965199185091c21737deff8caacae
Yoon321 <tkdwls734@gmail.com>
2022-05-03 17:50:13 +0900

    웹사이트 형태 만들기

--- project03/app.py
+++ project03/app.py
@@ -12,7 +12,10 @@ def main():
 
 @app.route('/matjip', methods=['GET'])
 def get_matjip():
-    return jsonify({'result': 'success', 'matjip_list':[]})
+    matjip_list = list(db.matjip.find({},{'_id':False}));
+
+    return jsonify({'result': 'success', 'matjip_list': matjip_list})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
