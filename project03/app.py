48302ee1d5a3018505c9829f7d692777a52d069a
Yoon321 <tkdwls734@gmail.com>
2022-05-03 14:34:22 +0900

    네이버 지도 API 연습하
    화면에 지도 보여주기, 마커 생성하기, info 생성하기, 마커 변경하기

--- project03/app.py
+++ project03/app.py
@@ -8,7 +8,7 @@ db = client.dbsparta_plus_week3
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('prac_map.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
